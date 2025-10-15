f8dd9668bb829515caf43192e6964946700364bf
Jeremías <jeremias.rootstrap@gmail.com>
2025-10-15 17:25:31 -0300

    docker improve install

--- root/bin/terraform-install.sh
+++ root/bin/terraform-install.sh
@@ -5,6 +5,9 @@ ARCH=$(dpkg --print-architecture)
 
 cd /root
 
+tmpdir=$(mktemp -d /tmp/devops-install-terraform.XXXXXXX)
+cd "${tmpdir}"
+
 wget -O terraform.zip \
     "https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_${ARCH}.zip"
 
@@ -12,6 +15,11 @@ unzip terraform.zip
 
 install -o root -g root -m 0755 ./terraform /usr/local/bin/terraform
 
+rm -f terraform.zip terraform
+
 terraform -install-autocomplete
+
+cd /root
+rm -rf "${tmpdir}"
 
 exit 0
